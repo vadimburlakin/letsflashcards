254cf1cc0ff3ac516e98ad50f56d5b28350e31e1
mt-vadim <vburlakin@moneytree.jp>
2021-12-29 13:02:49 +0900

    set up frontend routing

--- app/javascript/app/app.jsx
+++ app/javascript/app/app.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { BrowserRouter } from "react-router-dom";
+import { Routes ,Route } from 'react-router-dom';
+
+import Home from './home';
+import RegistrationPage from './registration/registration-page';
 
 export default function App() {
   return (
-    <div style={{marginTop: '100px'}}>Testing</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/sign-in" element={<RegistrationPage />} />
+        <Route exact path="/sign-up" element={<RegistrationPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
